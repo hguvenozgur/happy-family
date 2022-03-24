6b37b596f02c445ba8d100103c380feefdc02a52
Guven Ozgur <gwenozgur@gmail.com>
2022-03-24 13:54:43 +0300

    Initial commit

---
+++ src/main/java/com/java/happyfamily/HappyFamilyApplication.java
@@ -0,0 +1,13 @@
+package com.java.happyfamily;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HappyFamilyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HappyFamilyApplication.class, args);
+	}
+
+}
